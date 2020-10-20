402d0b944a36aed222b7e83a7f9bebd0871451f7
Ivan583 <ivanych2293@gmail.com>
2020-10-20 17:19:14 +0300

    Подготовка компонента App

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,37 @@
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
+    </div>-->
+    <router-view />
   </div>
 </template>
 
 <style>
+@import url("/reset.css");
+
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  min-height: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: rgb(216, 226, 245);
 }
 
+@media (min-width: 1400px) {
+  #app {
+    max-width: 1400px;
+    margin: 0 auto;
+    border: 5px double green;
+    border-bottom: none;
+  }
+}
 </style>
