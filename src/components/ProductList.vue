7236c603e405c00e4f58e047ef2254b7bd480ace
Ivan583 <ivanych2293@gmail.com>
2020-10-20 19:06:44 +0300

    Создание компонента списка
    Создание компонента ProductList и его тестирование

---
+++ src/components/ProductList.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    {{kit}}
+    <!-- <div v-for="product in products" :key="product.id">
+        <figure>
+          <img :src="product.image" :alt="product.alt" class="figure" />
+          <figcaption>
+            <div class="data">
+              <div class="title">
+                <p v-text="product.title"></p>
+              </div>
+              <div class="description">
+                <p v-html="product.description"></p>
+              </div>
+            </div>
+            <div>
+              <div class="price">
+                <p>
+                  Price:
+                  <span>{{product.price | formatPrice}}</span>
+                </p>
+              </div>
+              <div class="rating">
+                <span v-for="n in 5" :class="{'active-star': starRating(n, product)}" :key="n.id">☆</span>
+              </div>
+            </div>
+          </figcaption>
+        </figure>
+    </div>-->
+  </main>
+</template>
+
+<script>
+export default {
+  props: ["kit"]
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,12 @@
 <template>
   <div class="page">
     <h1>Список товаров</h1>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <main>
-      <div v-for="product in products" :key="product.id">
-        <figure>
-          <img :src="product.image" :alt="product.alt" class="figure" />
-          <figcaption>
-            <div class="data">
-              <div class="title">
-                <p v-text="product.title"></p>
-              </div>
-              <div class="description">
-                <p v-html="product.description"></p>
-              </div>
-            </div>
-            <div>
-              <div class="price">
-                <p>
-                  Price:
-                  <span>{{product.price | formatPrice}}</span>
-                </p>
-              </div>
-              <div class="rating">
-                <span v-for="n in 5" :class="{'active-star': starRating(n, product)}" :key="n.id">☆</span>
-              </div>
-            </div>
-          </figcaption>
-        </figure>
-      </div>
-    </main>
+    <ProductList :kit="products" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import ProductList from "@/components/ProductList.vue";
 
 export default {
   name: "Home",
@@ -45,7 +16,7 @@ export default {
     };
   },
   components: {
-    // HelloWorld
+    ProductList
   },
 
   methods: {
