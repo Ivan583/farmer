16ac250b732f7de0e40a091e111b443066b950cd
Ivan583 <ivanych2293@gmail.com>
2020-10-20 21:01:32 +0300

    Добавление страниц

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,38 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/winter',
+    name: 'Winter',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (winter.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "winter" */ '../views/Winter.vue')
+  },
+  {
+    path: '/spring',
+    name: 'Spring',
+    component: () => import('../views/Spring.vue')
+  },
+  {
+    path: '/fertil',
+    name: 'Fertilizers',
+    component: () => import('../views/Fertilizers.vue')
+  },
+  {
+    path: '/tools',
+    name: 'Tools',
+    component: () => import('../views/Tools.vue')
+  },
+  {
+    path: '/mechan',
+    name: 'Mechanisms',
+    component: () => import('../views/Mechanisms.vue')
   }
 ]
 
