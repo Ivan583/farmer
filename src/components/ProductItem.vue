13f5c91c6acec511674be8f5d0a1b1fe47f0b73c
Ivan583 <ivanych2293@gmail.com>
2020-10-20 19:19:13 +0300

    Создание компонента ProductItem
    Создание компонента ProductItem и его тестирование

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <figure>
+    {{ product.description }}
+    <!-- <img :src="product.image" :alt="product.alt" class="figure" />
+        <figcaption>
+          <div class="data">
+            <div class="title">
+              <p v-text="product.title"></p>
+            </div>
+            <div class="description">
+              <p v-html="product.description"></p>
+            </div>
+          </div>
+          <div>
+            <div class="price">
+              <p>
+                Price:
+                <span>{{product.price | formatPrice}}</span>
+              </p>
+            </div>
+            <div class="rating">
+              <span v-for="n in 5" :class="{'active-star': starRating(n, product)}" :key="n.id">☆</span>
+            </div>
+          </div>
+    </figcaption>-->
+  </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,38 +1,14 @@
 <template>
   <main>
-    {{kit}}
-    <!-- <div v-for="product in products" :key="product.id">
-        <figure>
-          <img :src="product.image" :alt="product.alt" class="figure" />
-          <figcaption>
-            <div class="data">
-              <div class="title">
-                <p v-text="product.title"></p>
-              </div>
-              <div class="description">
-                <p v-html="product.description"></p>
-              </div>
-            </div>
-            <div>
-              <div class="price">
-                <p>
-                  Price:
-                  <span>{{product.price | formatPrice}}</span>
-                </p>
-              </div>
-              <div class="rating">
-                <span v-for="n in 5" :class="{'active-star': starRating(n, product)}" :key="n.id">☆</span>
-              </div>
-            </div>
-          </figcaption>
-        </figure>
-    </div>-->
+    <ProductItem v-for="elem in kit" :key="elem.id" :product="elem" />
   </main>
 </template>
 
 <script>
+import ProductItem from "@/components/ProductItem.vue";
 export default {
-  props: ["kit"]
+  props: ["kit"],
+  components: { ProductItem }
 };
 </script>
 
