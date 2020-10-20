44a61958861f5225813ab1d730a1b2d0f283fd54
Ivan583 <ivanych2293@gmail.com>
2020-10-20 19:25:43 +0300

    Передача данных в компонент ProductItem

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,28 +1,27 @@
 <template>
   <figure>
-    {{ product.description }}
-    <!-- <img :src="product.image" :alt="product.alt" class="figure" />
-        <figcaption>
-          <div class="data">
-            <div class="title">
-              <p v-text="product.title"></p>
-            </div>
-            <div class="description">
-              <p v-html="product.description"></p>
-            </div>
-          </div>
-          <div>
-            <div class="price">
-              <p>
-                Price:
-                <span>{{product.price | formatPrice}}</span>
-              </p>
-            </div>
-            <div class="rating">
-              <span v-for="n in 5" :class="{'active-star': starRating(n, product)}" :key="n.id">☆</span>
-            </div>
-          </div>
-    </figcaption>-->
+    <img :src="product.image" :alt="product.alt" class="figure" />
+    <figcaption>
+      <div class="data">
+        <div class="title">
+          <p v-text="product.title"></p>
+        </div>
+        <div class="description">
+          <p v-html="product.description"></p>
+        </div>
+      </div>
+      <div>
+        <div class="price">
+          <p>
+            Price:
+            <span>{{product.price | formatPrice}}</span>
+          </p>
+        </div>
+        <div class="rating">
+          <span v-for="n in 5" :class="{'active-star': starRating(n, product)}" :key="n.id">☆</span>
+        </div>
+      </div>
+    </figcaption>
   </figure>
 </template>
 
@@ -32,6 +31,29 @@ export default {
     product: {
       type: Object,
       required: true
+    }
+  },
+
+  methods: {
+    starRating(n, myProduct) {
+      return myProduct.rating - n >= 0;
+    }
+  },
+
+  filters: {
+    formatPrice(price) {
+      if (price < 1000) {
+        return "$ " + price.toFixed(2);
+      } else {
+        let priceString = price.toFixed(2);
+        let priceArray = priceString.split("").reverse();
+        let index = 3;
+        while (priceArray.length > index + 3) {
+          priceArray.splice(index + 3, 0, " ");
+          index += 4;
+        }
+        return "$ " + priceArray.reverse().join("");
+      }
     }
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,29 +19,6 @@ export default {
     ProductList
   },
 
-  methods: {
-    starRating(n, myProduct) {
-      return myProduct.rating - n >= 0;
-    }
-  },
-
-  filters: {
-    formatPrice(price) {
-      if (price < 1000) {
-        return "$ " + price.toFixed(2);
-      } else {
-        let priceString = price.toFixed(2);
-        let priceArray = priceString.split("").reverse();
-        let index = 3;
-        while (priceArray.length > index + 3) {
-          priceArray.splice(index + 3, 0, " ");
-          index += 4;
-        }
-        return "$ " + priceArray.reverse().join("");
-      }
-    }
-  },
-
   mounted() {
     fetch("/products.json")
       .then(res => res.json())
