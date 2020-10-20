f8a36526f1e1f335913cbfd3a605d4e9c219fde9
Ivan583 <ivanych2293@gmail.com>
2020-10-20 21:14:53 +0300

    Добавление анимации
    Добавление анимации при открытии главной страницы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="page">
     <h1>Список товаров</h1>
-    <ProductList :kit="products" />
+    <Loader v-if="loading" />
+    <ProductList v-else :kit="products" />
   </div>
 </template>
 
 <script>
 import ProductList from "@/components/ProductList.vue";
+import Loader from "@/components/Loader.vue";
 
 export default {
   name: "Home",
   data() {
     return {
-      products: []
+      products: [],
+      loading: true
     };
   },
   components: {
-    ProductList
+    ProductList,
+    Loader
   },
 
   mounted() {
     fetch("/products.json")
       .then(res => res.json())
       .then(data => {
-        this.products = data.products;
+        setTimeout(() => {
+          this.products = data.products;
+          this.loading = false;
+        }, 2000);
       })
       .catch(err => {
         console.log(err);
