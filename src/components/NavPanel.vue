ca80bbcd3d4541ba8ca9a5bafe909e5e908fe776
Ivan583 <ivanych2293@gmail.com>
2020-10-21 17:38:31 +0300

    Отделение большой панели
    Вывод большой панели в компонент BigMenu

--- src/components/NavPanel.vue
+++ src/components/NavPanel.vue
@@ -22,30 +22,16 @@
       </div>
     </div>
 
-    <div class="big-menu">
-      <ul>
-        <li>
-          <router-link to="/">На главную</router-link>
-        </li>
-        <li>
-          <router-link to="/winter">Озимые культуры</router-link>
-        </li>
-        <li>
-          <router-link to="/spring">Яровые культуры</router-link>
-        </li>
-        <li>
-          <router-link to="/fertil">Удобрения</router-link>
-        </li>
-        <li>
-          <router-link to="/tools">Инструменты</router-link>
-        </li>
-        <li>
-          <router-link to="/mechan">Механизмы</router-link>
-        </li>
-      </ul>
-    </div>
+    <BigMenu />
   </div>
 </template>
+
+<script>
+import BigMenu from "@/components/BigMenu.vue";
+export default {
+  components: { BigMenu }
+};
+</script>
 
 <style scoped>
 .box {
@@ -148,38 +134,9 @@ a {
   transform: rotate(-45deg);
 }
 
-/* Большая панель  */
-
-.big-menu {
-  display: none;
-}
-
 @media (min-width: 600px) {
   .box {
     display: none;
-  }
-
-  .big-menu {
-    display: block;
-    margin: 15px 0 0 70px;
-  }
-
-  .big-menu li {
-    display: inline-block;
-    margin: 0 5px 5px 0;
-  }
-
-  .big-menu li a {
-    display: inline-block;
-    width: 196px;
-    color: red;
-    background: rgb(90, 206, 90);
-    font-family: Arial, Helvetica, sans-serif;
-    text-decoration: none;
-    font-size: 1.2em;
-    font-weight: bold;
-    text-align: center;
-    height: 50px;
   }
 }
 </style>
